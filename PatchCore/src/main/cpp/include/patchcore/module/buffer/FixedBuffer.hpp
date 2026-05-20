4082c8bb5d51d3577af532b743bcdb2818ebd1c5
Evgenii Petrov <geka000@gmail.com>
2026-05-20 12:41:18 +0200

    refactor: rename envelope method to processSample in multiple modules

---
+++ PatchCore/src/main/cpp/include/patchcore/module/buffer/FixedBuffer.hpp
@@ -0,0 +1,9 @@
+#ifndef PATCHCORE_FIXEDBUFFER_HPP
+#define PATCHCORE_FIXEDBUFFER_HPP
+
+#include "patchcore/module/buffer/BlockConfig.hpp"
+#include <array>
+
+using FixedBuffer = std::array<float, PATCHCORE_BLOCK_SIZE>;
+
+#endif //PATCHCORE_FIXEDBUFFER_HPP
